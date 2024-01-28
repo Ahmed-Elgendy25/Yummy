b196ff58c2ea35b06bd4ae463b45e0fd9b6ebb14
SugarAbuserr <202123713@o6u.edu.eg>
2024-01-28 18:19:39 +0200

    final-version-2

--- JS/sidenav.js
+++ JS/sidenav.js
@@ -1,26 +1,5 @@
-// $('#sidebar-btn').on('click', function () {
-//   $('#sidebar').toggleClass('visible');
-
-//   if ($('#sidebar').hasClass('visible')) {
-//     $('#nav-link').slideDown(1000);
-//     $('#sidebar-outter').animate({ width: '60px', left: '12%' });
-//   } else {
-//     $('#nav-link').slideUp(1000);
-//     $('#sidebar-outter').animate({ width: '5%', left: '0' });
-//   }
-
-//   $('#sidebar-btn, #sidebar-exit').toggleClass('d-none');
-// });
-
-// $('#sidebar-exit').on('click', function () {
-//   $('#sidebar').toggleClass('visible');
-
-//   $('#nav-link').slideUp(1000);
-//   $('#sidebar-btn, #sidebar-exit').toggleClass('d-none');
-//   $('#sidebar-outter').animate({ width: '5%', left: '0' });
-// });
-
 $(function () {
+  $('#sidebar-outter').animate({ left: 0 });
   $('#sidebar-btn').on('click', function () {
     $('#sidebar').toggleClass('visible');
     if ($('#sidebar').hasClass('visible')) {
